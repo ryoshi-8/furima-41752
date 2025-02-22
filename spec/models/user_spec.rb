5ca3cc65feb693a226aacbc2f5064a8f1582994d
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-02-23 06:32:23 +0900

    テストコードの修正（password）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,9 +45,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは、半角英数字混合でなければ登録できない' do
+      it '数字のみのpasswordでは登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it '英字のみのpasswordでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it '全角数字を含むpasswordでは登録できない' do
+        @user.password = '１２３４５６'
+        @user.password_confirmation = '１２３４５６'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
