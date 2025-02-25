022e82669d5671bf7d06ead6717f5eda3b0434a3
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-02-25 17:40:47 +0900

    ActiveHash導入、バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :ship_fee_burden
+  belongs_to :prefecture
+  belongs_to :days_until_ship
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :ship_fee_burden_id
+    validates :prefecture_id
+    validates :days_until_ship_id
+  end
 end
