92b522edf836ed989344cbe36a616a02d607d2c5
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-02-27 17:45:29 +0900

    商品出品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,17 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :ship_fee_burden_id,
+                                 :prefecture_id, :days_until_ship_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,16 +12,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product_name
     validates :description
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :ship_fee_burden_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :days_until_ship_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :price,
               numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000,
                               message: '300から9999999の間の半角数値を使用してください' }
-  end
-
-  with_options numericality: { other_than: 1, message: "can't be blank" } do
-    validates :category_id
-    validates :condition_id
-    validates :ship_fee_burden_id
-    validates :prefecture_id
-    validates :days_until_ship_id
   end
 end
