4e31ff277bd7628d0125cd2ab84099de5a004a00
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-03-02 06:18:06 +0900

    販売手数料等の計算表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    profitDom.innerHTML = inputValue - Math.floor(inputValue * 0.1)
+  })
+ };
+ 
+ window.addEventListener("turbo:load", price);
+ window.addEventListener("turbo:render", price);
