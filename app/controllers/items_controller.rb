826bba874d62637aa1b2ba721dda19b21c17b185
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-03-03 06:23:58 +0900

    items_controllerの修正(private追加)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,8 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
+
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :ship_fee_burden_id,
                                  :prefecture_id, :days_until_ship_id, :price).merge(user_id: current_user.id)
