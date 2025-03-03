a97bd4f759fbf86a51208ee7eb661a0e251b125a
ryoshi-8 <rsy3ryo248sa@gmail.com>
2025-03-03 14:40:06 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :days_until_ship_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :price,
-              numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000,
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: 'は300から9999999の間の半角数値を使用してください' }
   end
 end
